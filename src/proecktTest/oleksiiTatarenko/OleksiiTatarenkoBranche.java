ef5c685379f969be6af6d38163c5a4b0825bf5c1
aleks <aht477@gmail.com>
2025-01-19 16:59:51 +0100

    New Branch Oleksii Tatarenko

--- src/proecktTest/oleksiiTatarenko/OleksiiTatarenkoBranche.java
+++
@@ -1,7 +0,0 @@
-package proecktTest.oleksiiTatarenko;
-
-public class OleksiiTatarenkoBranche {
-    public static void main(String[] args) {
-        System.out.println("Tatarenko Oleksii");
-    }
-}
